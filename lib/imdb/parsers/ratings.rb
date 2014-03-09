99879fe55701bcab6e117451f33189d8d6b2aaec
Anup Narkhede <anup.narkhede@gmail.com>
2014-03-09 18:33:30 +0000

    Handle non UTF-8 characters gracefully

--- lib/imdb/parsers/ratings.rb
+++ lib/imdb/parsers/ratings.rb
@@ -4,7 +4,8 @@ module Imdb::Parsers
     REGEX = /^(\d\.\d)([^(]*)(\([\d]{4}\))(.*)/
 
     def parse(string)
-      if match = string[27..-1].match(REGEX)
+      source = string[27..-1].encode!('UTF-8', 'UTF-8', :invalid => :replace)
+      if match = source.match(REGEX)
         Imdb::Movie.new(
           sanitize_name(match[2]),
           sanitize_rating(match[1]),
