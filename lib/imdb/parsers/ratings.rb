cd9a2a951adf7a54be0d2e1b4bca0acd715ce598
Anup Narkhede <anup.narkhede@gmail.com>
2014-03-09 22:47:08 +0000

    Rescue unknown errors

--- lib/imdb/parsers/ratings.rb
+++ lib/imdb/parsers/ratings.rb
@@ -4,14 +4,18 @@ module Imdb::Parsers
     REGEX = /^(\d\.\d)([^(]*)(\([\d]{4}\))(.*)/
 
     def parse(string)
-      source = string[27..-1].encode!('UTF-8', 'UTF-8', :invalid => :replace)
-      if match = source.match(REGEX)
-        Imdb::Movie.new(
-          sanitize_name(match[2]),
-          sanitize_rating(match[1]),
-          sanitize_year(match[3]),
-          sanitize_desc(match[4])
-        )
+      begin
+        source = string[27..-1].encode!('UTF-8', 'UTF-8', :invalid => :replace)
+        if match = source.match(REGEX)
+          Imdb::Movie.new(
+            sanitize_name(match[2]),
+            sanitize_rating(match[1]),
+            sanitize_year(match[3]),
+            sanitize_desc(match[4])
+          )
+        end
+      rescue
+        nil
       end
     end
   end
